e21a218796bb9f9421f89f5a37841930b25670cb
NicholasLina <nicktlina@gmail.com>
2024-07-28 13:52:44 -0400

    add avatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import styles from "../styles/Header.module.css"
+import AVATAR from "../assets/avatar.svg"
 
 export default function Header() {
     return(
         <div className={ styles.header }>
             <h1>Developer, Designer + Digital Artist</h1>
             <p className={ styles.tagline }>Code. Craftsmanship. Creation.</p>
+            <div className={ styles.avatar }>
+                <img src={ AVATAR }></img>
+            </div>
         </div>
     )
 }
