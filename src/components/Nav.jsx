7d508f0da4db588c26fbcaa5a56bf91bec4da239
NicholasLina <nicktlina@gmail.com>
2024-10-02 15:33:22 -0400

    add github to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from "../styles/Nav.module.css"
-import { FaCodepen, FaLinkedin } from "react-icons/fa6";
+import { FaCodepen, FaGithub, FaLinkedin } from "react-icons/fa6";
 
 const Nav = () => {
     return (
         <div className={ styles.nav }>
             <a className={ styles.logo }>Nick Lina</a>
             <div className={ styles.links }>
+                <a href="https://github.com/NicholasLina/"><FaGithub /></a>
                 <a href="https://www.linkedin.com/in/nick-lina-87bb92164/"><FaLinkedin /></a>
                 <a href="https://codepen.io/nicklina"><FaCodepen /></a>
             </div>
