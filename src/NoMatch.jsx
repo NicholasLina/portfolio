3f1aaa7e618a6d8a87f40edbe9333c50a2586ec1
NicholasLina <nicktlina@gmail.com>
2024-10-02 15:37:44 -0400

    update 404 page style

--- src/NoMatch.jsx
+++ src/NoMatch.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import styles from "./styles/NoMatch.module.css"
 
 export default function NoMatch() {
     return (
-        <div>
+        <div className={ styles.noMatch }>
             <h2>Congratulations! You found a LUCKY LINK (also known as a 404 error)</h2>
             <p>
                 <Link to="/">Go to the home page</Link>
